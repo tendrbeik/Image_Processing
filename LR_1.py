1c06967778a621d7d133d72db3f6ce2804d42361
tendrbeik <abdramanov-2014@mail.ru>
2024-09-06 07:47:39 +0300

    Добавлена первая работа

---
+++ LR_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+A = np.ones(3)*1
+#A = np.array([1,2,3],int)
+B = np.ones(3)*2
+#print(A)
+#print(B)
+#print(np.add(A,B))
+#print(np.divide(-A,2))
+print(np.add(A,B)*np.divide(-A,2))
