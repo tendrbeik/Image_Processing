cffebcf756291612d1b1d7231868b6eec6d6eb0c
tendrbeik <abdramanov-2014@mail.ru>
2024-09-17 19:59:08 +0300

    Исправлена 4-ая работа

--- LR_4.py
+++ LR_4.py
@@ -21,30 +21,43 @@ plt.xticks([])
 plt.yticks([])
 plt.show()
 
-#Применяем метод Кани
-threshold1 = 10
-threshold2 = 20
-edges = cv.Canny(thresh1, threshold1, threshold2)
+# #Применяем метод Кани
+# threshold1 = 10
+# threshold2 = 20
+# edges = cv.Canny(thresh1, threshold1, threshold2)
 
-#Применяем инверсию
-lut = lambda i: 255 - i
-edges = lut(edges)
-plt.figure(figsize=(20, 20))
-plt.imshow(edges, 'gray')
-plt.show()
+# #Применяем инверсию
+# lut = lambda i: 255 - i
+# thresh1 = lut(thresh1)
+# plt.figure(figsize=(20, 20))
+# plt.imshow(thresh1, 'gray')
+# plt.show()
 
 #Применяем гауссовское размытие
 kernel55 = np.ones((5, 5), np.float32) / 25
 kernel77 = np.ones((7, 7), np.float32) / 49
+kernel88 = np.ones((8, 8), np.float32) / 64
 
-filtered_image = cv.filter2D(edges, -1, kernel77)
+filtered_image = cv.filter2D(thresh1, -1, kernel77)
 # filtered_image = cv.medianBlur(thresh1, 5)
 plt.figure(figsize=(20, 20))
 plt.imshow(filtered_image, 'gray')
 plt.show()
 
+#Проводим бинаризацию
+threshold = 234
+
+ret1, thresh1 = cv.threshold(filtered_image, threshold, 255, cv.THRESH_BINARY)
+#ret1, thresh1 = cv.threshold(filtered_image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+plt.figure(figsize=(12, 12))
+plt.imshow(thresh1, 'gray', vmin=0, vmax=255)
+plt.title("Binarization (threshold = %d)" % ret1)
+plt.xticks([])
+plt.yticks([])
+plt.show()
+
 #Меняем кодировку
-filtered_image = cv.cvtColor(filtered_image, cv.COLOR_GRAY2RGB)
+thresh1 = cv.cvtColor(thresh1, cv.COLOR_GRAY2RGB)
 #Сохраняем результат
-plt.imsave("images/as2.png", filtered_image)
+plt.imsave("images/as2.png", thresh1)
 
